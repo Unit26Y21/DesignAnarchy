d14357da1b0960798269c7b9103706b075b276b7
MegKalinowski <92740557+MegKalinowski@users.noreply.github.com>
2022-02-21 18:59:42 -0500

    Update ROI_Calculator-MK.py

--- Spring/Teams/Air Rights/MyProject/ROI_Calculator-MK.py
+++ Spring/Teams/Air Rights/MyProject/ROI_Calculator-MK.py
@@ -1,5 +1,5 @@
-### 1. Research what is a back of the envelope proforma (or a simple ROI)
-    
+### 1. Research what is a back of the envelope pro forma (or a simple ROI)
+    #Back of envelope
 ### 2 . Write a series of python functions that take inputs used to calculated value and return you a simple ROI .###
 
 
@@ -10,11 +10,12 @@ Investment = 40000
 Rent = 700
 Loss = 1000
 
-#defining the investments can have breakdown of liquid capital, loans, grants, etc.
-def Investment
+#defining the investments can have breakdown of liquid capital, loans, bonds, , etc.
+#def Investment (Cash, Loans, Bonds,
+#   Incremental Funding: from NYC + HUD, 10-year 2.2 billion dollars starting in 2019)
 
 #defining Rent can be a culmination of number of units and rent per type
-def Rent
+#def Rent (NumUnits,
 
 #defining ROI as a rudimentary gauge of an investment’s profitability
 def ROI (Investment, Rent, Loss):
@@ -25,3 +26,7 @@ def ROI (Investment, Rent, Loss):
 ROI (Investment, Rent, Loss)
 
 #Rate of Return- takes into account the project's time frame
+#SROI- Social return on investment, to understand the environmental, social and governance (ESG) criteria used in Socially responsible Investing (SRI)
+
+
+##
