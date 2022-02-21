543eb573f913d37edb20aa9431c744cc1234bc6d
anaace1 <aaceved006@citymail.cuny.edu>
2022-02-21 18:55:53 -0500

    ROI Calculator + Investment

--- Spring/Teams/Air Rights/MyProject/ROI_Calculator-MK.py
+++ Spring/Teams/Air Rights/MyProject/ROI_Calculator-MK.py
@@ -6,17 +6,40 @@
 #Creating ROI Calculator#
 
 #Define the Variables#
-Investment = 40000
+#Investment = 40000
 Rent = 700
 Loss = 1000
 
 #defining the investments can have breakdown of liquid capital, loans, grants, etc.
-def Investment
 
-#defining Rent can be a culmination of number of units and rent per type
+investmentDictionary = {}
+
+###Ownership Investment Variables###
+stock= 2000 #Suppose is the value of Housing Authority
+business= None
+realEstate= 10000 #Suppose is LandValue
+objectsCollectibles= None
+
+###Lending Investment Variables###
+savingAccount= 5000
+bonds= 20000 #Suppose is a loan from BoFA
+
+###Cash Equivalent Investment Variables###
+moneyMarket= None
+
+for variable in ["stock", "business", "realEstate", "objectsCollectibles", "savingAccount", "bonds", "moneyMarket"]:
+    investmentDictionary[variable] = eval(variable)
+
+def sumOfInvestments (investmentDictionary):
+    Investment= sum(investmentDictionary)
+#    print(Investment)
+
+"""
+    #defining Rent can be a culmination of number of units and rent per type
 def Rent
+"""
 
-#defining ROI as a rudimentary gauge of an investment’s profitability
+    # defining ROI as a rudimentary gauge of an investment’s profitability
 def ROI (Investment, Rent, Loss):
     NetProfit = Rent * 12 - Loss
     ROI = (NetProfit/Investment) *100
