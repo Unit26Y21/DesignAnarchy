ad8a90472bb3de3c9f72fd14235749c7a684579a
MegKalinowski <92740557+MegKalinowski@users.noreply.github.com>
2022-02-22 09:15:21 -0500

    Updates to define Back of Envelope

--- Spring/Teams/Air Rights/MK-Test/ROI_Calculator-MK.py
+++ Spring/Teams/Air Rights/MK-Test/ROI_Calculator-MK.py
@@ -1,14 +1,7 @@
 ### 1. Research what is a back of the envelope pro forma (or a simple ROI)
-    #Back of envelope
+    #Back of envelope is a rough estimate that filters out less desirable deals
 ### 2 . Write a series of python functions that take inputs used to calculated value and return you a simple ROI .###
 
-
-#Creating ROI Calculator#
-
-#Define the Variables#
-Investment = 40000
-Rent = 700
-Loss = 1000
 
 #defining the investments can have breakdown of liquid capital, loans, bonds, , etc.
 #def Investment (Cash, Loans, Bonds,
@@ -16,6 +9,8 @@ Loss = 1000
 
 #defining Rent can be a culmination of number of units and rent charged per UnitType
 #AMI= AreaMediumIncome
+##Source:
+# https://www.elliman.com/resources/siteresources/commonresources/static%20pages/images/corporate-resources/q3_2021/rental-09_2021.pdf
     #SeniorHousing:
         #UnitA1 = SeniorStudioUnitAffordable, PricePerUnitA1 = 30%AMI ----->
             # OR BETTER way to calculate this?? Set a range from 30-100%AMI,
@@ -42,29 +37,37 @@ Loss = 1000
 
     #def Rent (NumUnitA * PricePerUnit + )
 
-investmentDictionary = {}
+#investmentDictionary = {}
 
 ###Ownership Investment Variables###
-stock= 2000 #Suppose is the value of Housing Authority
-business= None
-realEstate= 10000 #Suppose is LandValue
-objectsCollectibles= None
+#stock= 2000 #Suppose is the value of Housing Authority
+#business= None
+#realEstate= 10000 #Suppose is LandValue
+#objectsCollectibles= None
 
 ###Lending Investment Variables###
-savingAccount= 5000
-bonds= 20000 #Suppose is a loan from BoFA
+#savingAccount= 5000
+#bonds= 20000 #Suppose is a loan from BoFA
 
 ###Cash Equivalent Investment Variables###
-moneyMarket= None
+#moneyMarket= None
 
 
-for variable in ["stock", "business", "realEstate", "objectsCollectibles", "savingAccount", "bonds", "moneyMarket"]:
-    investmentDictionary[variable] = eval(variable)
+#for variable in ["stock", "business", "realEstate", "objectsCollectibles", "savingAccount", "bonds", "moneyMarket"]:
+   # investmentDictionary[variable] = eval(variable)
 
 
-def sumOfInvestments (investmentDictionary):
-    Investment= sum(investmentDictionary)
+#def sumOfInvestments (investmentDictionary):
+   # Investment= sum(investmentDictionary)
 #    print(Investment)
+
+
+###Creating ROI Calculator###
+
+#Define the Variables#
+Investment = 40000
+Rent = 700
+Loss = 1000
 
 #defining ROI as a rudimentary gauge of an investment’s profitability
 def ROI (Investment, Rent, Loss):
