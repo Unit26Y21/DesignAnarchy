e6b51b135275d84018bfbc017c1bbb77f8eb003a
sidnieee <73193991+sidnieee@users.noreply.github.com>
2022-02-28 00:48:26 -0500

    Test

--- Spring/Teams/Air Rights/HW1/HW1.py
+++ Spring/Teams/Air Rights/HW1/HW1.py
@@ -1,13 +1,11 @@
 CurrentValue = 40000
 InputCost = 7000
+Loss = 2000
 
-
-def ROI(CurrentValue, InputCost):
-    NetProfit = CurrentValue - InputCost
+def ROI(CurrentValue, InputCost, Loss):
+    NetProfit = CurrentValue * 12 - Loss
     ROI = (NetProfit / InputCost)*100
     print(ROI)
 
 
-ROI(CurrentValue, InputCost)
-
-#
+my_ROI = ROI(CurrentValue, InputCost, Loss)
