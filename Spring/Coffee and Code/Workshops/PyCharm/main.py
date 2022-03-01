97596a00090e9e68c37121487a0153c1e4bdb417
Héctor Tarrido-Picart <htarridopicart@ccny.cuny.edu>
2022-02-28 22:17:40 -0500

    Updated PyCharm folder
    - missing files for some reason

---
+++ Spring/Coffee and Code/Workshops/PyCharm/main.py
@@ -0,0 +1,8 @@
+import ProForma_ROI
+# import ROICalculator
+import classNo2
+
+if __name__ == '__main__':
+    # ProForma_ROI.userInput()
+    # ROICalculator.my_ROI
+    # classNo2 # collection ata types
