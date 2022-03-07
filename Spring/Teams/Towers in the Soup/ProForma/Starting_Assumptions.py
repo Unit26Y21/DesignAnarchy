4d4be3da49b09de77712b5597b898717aadb656a
kkenny000 <92530883+kkenny000@users.noreply.github.com>
2022-03-07 14:03:34 -0500

    Manufacturing Py Files
    initial edits

---
+++ Spring/Teams/Towers in the Soup/ProForma/Starting_Assumptions.py
@@ -0,0 +1,15 @@
+#Starting Assumptions Constants
+
+devTimeline = 11
+lotArea = 10375
+existingBuildingFloorArea =
+residentialFAR = 7.52
+commercialFAR = 1.00
+manufacturingFAR = 1.00
+residentialZoningFloorArea = 100000
+commercialZoningFloorArea = 100000
+manufacturingZoningFloorArea = 100000
+airRightsSqft = 0
+innacuracyFactor = .03
+inaccuracyFloorLoss = 9000
+netLossFactor = .15
