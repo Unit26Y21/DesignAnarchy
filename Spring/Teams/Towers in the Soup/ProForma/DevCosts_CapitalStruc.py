e86eea1658c6044b7991ca0d5cbb791fd730a16d
RebeccaHager <rhager000@citymail.cuny.edu>
2022-03-13 20:56:18 -0400

    Redid Labels
    Updated what the components are named

--- Spring/Teams/Towers in the Soup/ProForma/DevCosts_CapitalStruc.py
+++ Spring/Teams/Towers in the Soup/ProForma/DevCosts_CapitalStruc.py
@@ -1,112 +1,122 @@
 import DevelopmentCostsInputs as devCost
-##push attempt##
 
 #0.StartingAssumptions#
 
 def TotalGrossZoningFloorArea(residZFA, comZFA, manZFA):
     totalGZFA = [residZFA,comZFA,manZFA]
     sumGZFA = sum(totalGZFA)
-    #for i in totalGZFA:
-    print("Residential", residZFA,)
-    print("Commercial", comZFA)
-    print("Manufacturing", manZFA)
+##for i in totalGZFA- how would we be able to it that way but print what each one is only one time##
+    print("ResidentialZoningFloorArea")
+    print(residZFA)
+    print("CommercialZoningFloorArea")
+    print(comZFA)
+    print("ManufacturingZoningFloorArea")
+    print(manZFA)
     return sumGZFA
 
 def InacuracyFloorLoss(myTotalGZFA, InacFact):
     floorLoss = myTotalGZFA * InacFact
-    print("Inaccuracy factor")
+    print("InaccuracyFactor")
     print(InacFact)
     return floorLoss
 
 def TotalZoningFloorArea(myTotalGZFA, LossFact):
     totalZFA = myTotalGZFA - (myTotalGZFA * LossFact)
-    print("Net loss factor")
+    print("NetLossFactor")
     print(LossFact)
     return totalZFA
 
 myTotalGZFA = TotalGrossZoningFloorArea(residZFA= devCost.ResidentialZoningFloorArea,
                                        comZFA= devCost.CommercialZoningFloorArea,
                                        manZFA= devCost.ManufacturingZoningFloorArea)
-print("My total gross zoning floor area")
+print("InitialTotalGrossZoningFloorArea")
 print(myTotalGZFA)
 
 myFloorLoss = InacuracyFloorLoss(myTotalGZFA= myTotalGZFA,
                                 InacFact= devCost.InacuracyFactor)
-print("my total floor loss")
+print("Inacuracy+/-FloorLoss")
 print(myFloorLoss)
 
 myTotalZFA = TotalZoningFloorArea(myTotalGZFA= myTotalGZFA,
                                   LossFact= devCost.NetLossFactor)
-print("My total zoning floor area")
+print("FinalTotalZoningFloorArea")
 print(myTotalZFA)
 
 #A.DevelopmentCosts#
 
 def LandPurchaseCost(landCost, lotArea):
     totalPurchCost = landCost * lotArea
+    print("LandCost")
     print(landCost)
+    print("LotArea")
     print(lotArea)
     return totalPurchCost
 
 def ResidentialCost(residZFA, residCost):
     totalResidCost = residZFA * residCost
+    print("ResidentialCostpsf")
     print(residCost)
     return totalResidCost
 
 def CommercialCost(comZFA, comCost):
     totalComCost = comZFA * comCost
+    print("CommercialCostpsf")
     print(comCost)
     return totalComCost
 
 def ManufacturingCost(manZFA, manCost):
     totalManCost = manZFA * manCost
+    print("ManufacturingCostpsf")
     print(manCost)
     return totalManCost
 
 def HardCost(myTotalGZFA, hardCost):
     totalHardCost = myTotalGZFA * hardCost
+    print("HardCostpsf")
     print(hardCost)
     return totalHardCost
 
 def SoftCost(myTotalGZFA, softCost):
     totalSoftCost = myTotalGZFA * softCost
+    print("SoftCostpsf")
     print(softCost)
     return totalSoftCost
 
 def TotalDevelopmentCost(BuildPurch, mytotalPurchCost, mytotalResidCost, mytotalComCost, mytotalManCost, mytotalHardCost, mytotalSoftCost):
     totalDevCost = [BuildPurch, mytotalPurchCost, mytotalResidCost, mytotalComCost, mytotalManCost, mytotalHardCost, mytotalSoftCost]
     sumDevCost: int = sum(totalDevCost)
+    print("ExistingBuildingPurchase")
     print(BuildPurch)
     return sumDevCost
 
 mytotalPurchCost = LandPurchaseCost(landCost= devCost.LandCostpsf,
                                     lotArea= devCost.LotArea)
-print("My total purchasing costs")
+print("TotalLandPurchaseCost")
 print(mytotalPurchCost)
 
 mytotalResidCost = ResidentialCost(residZFA= devCost.ResidentialZoningFloorArea,
                                    residCost= devCost.ResidentialCostpsf)
-print("My total residential Cost")
+print("TotalResidentialCost")
 print(mytotalResidCost)
 
 mytotalComCost = CommercialCost(comZFA= devCost.CommercialZoningFloorArea,
                                 comCost= devCost.CommercialCostpsf)
-print("My total commercial cost")
+print("TotalCommercialCost")
 print(mytotalComCost)
 
 mytotalManCost = ManufacturingCost(manZFA= devCost.ManufacturingZoningFloorArea,
                                    manCost= devCost.ManufacturingCostpsf)
-print("My total manufacturing cost")
+print("TotalManufacturingCost")
 print(mytotalManCost)
 
 mytotalHardCost = HardCost(myTotalGZFA= myTotalGZFA,
                            hardCost= devCost.HardCostpsf)
-print("My total hard cost")
+print("TotalHardCost")
 print(mytotalHardCost)
 
 mytotalSoftCost = SoftCost(myTotalGZFA= myTotalGZFA,
                            softCost= devCost.SoftCostpsf)
-print("My total soft cost")
+print("TotalSoftCost")
 print(mytotalSoftCost)
 
 myTotalDevCost = TotalDevelopmentCost(BuildPurch= devCost.ExistingBuildingPurchase,
@@ -116,13 +126,14 @@ myTotalDevCost = TotalDevelopmentCost(BuildPurch= devCost.ExistingBuildingPurcha
                                       mytotalManCost= mytotalManCost,
                                       mytotalHardCost= mytotalHardCost,
                                       mytotalSoftCost= mytotalSoftCost)
-print("My total development cost")
+print("TotalDevelopmentCost")
 print(myTotalDevCost)
 
 #B.CapitalStructure#
 
 def Equity(myTotalDevCost, EqPerc):
     totalEq = myTotalDevCost * EqPerc
+    print("EquityPercentage")
     print(EqPerc)
     return totalEq
 
@@ -132,22 +143,23 @@ def Debt(myTotalDevCost, mytotalEq):
 
 def DebtService(mytotalDebt, DebtServPerc):
     totalDebtServ = mytotalDebt * DebtServPerc
+    print("DebtServicePercentage")
     print(DebtServPerc)
     return totalDebtServ
 
 mytotalEq = Equity(myTotalDevCost= myTotalDevCost,
                        EqPerc= devCost.EquityPercentage)
-print("My total equity")
+print("TotalEquity")
 print(mytotalEq)
 
 mytotalDebt = Debt(myTotalDevCost= myTotalDevCost,
                    mytotalEq= mytotalEq)
-print("My total debt")
+print("TotalDebt")
 print(mytotalDebt)
 
 mytotalDebtServ = DebtService(mytotalDebt= mytotalDebt,
                               DebtServPerc= devCost.DebtServicePercentage)
-print("My total debt servicing cost")
+print("TotalDebtServicingCost")
 print(mytotalDebtServ)
 
 #C.ProjectedResidentialProceeds#
