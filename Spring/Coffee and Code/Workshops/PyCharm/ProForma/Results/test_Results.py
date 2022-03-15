c11c74bf7998798687d31c785b6e0d88a2322151
Héctor Tarrido-Picart <htarridopicart@ccny.cuny.edu>
2022-03-15 11:13:29 -0400

    Updated results file

---
+++ Spring/Coffee and Code/Workshops/PyCharm/ProForma/Results/test_Results.py
@@ -0,0 +1,13 @@
+import Results
+
+futureCashFlowTest = [880323,956599,1033967,1112448,1192064,1272838,1354794,1437953,1522342,24248132]
+
+testResults = Results(equity = 9712500,
+                      net_operating_income= 1995000,
+                      future_cash_flow_list= futureCashFlowTest,
+                      cash_flow_after_taxes= 880323,
+                      total_net_operating_income= 2431894,
+                      total_development_cost= 27750000,
+                      total_replacement_reserve= 1084022 ,
+                      accumulated_depreciation= -8310023,
+                      mortgage_payoff= -11853140)
